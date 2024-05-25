498f04a1be949d1dab9f6911b321ec75478119d2
abhishek6969 <sunkaleabhishek@gmail.com>
2024-05-25 13:15:18 +0530

    delete resources

--- Terraform/kv.tf
+++
@@ -1,4 +0,0 @@
-data "azurerm_key_vault_secret" "VM-default-password" {
-  name         = "VM-default-password"
-  key_vault_id = "/subscriptions/bed9c8b2-bb60-492d-92a9-d1641fb7adf8/resourceGroups/remote-backend/providers/Microsoft.KeyVault/vaults/terraform-kv-lirook"
-}
