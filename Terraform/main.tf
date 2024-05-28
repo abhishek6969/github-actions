d23a0282f128432df8bd087c8601d47de329a94f
abhishek6969 <sunkaleabhishek@gmail.com>
2024-05-28 15:47:23 +0530

    rg deployment

---
+++ Terraform/main.tf
@@ -0,0 +1,4 @@
+resource "azurerm_resource_group" "lirookRG" {
+  name     = var.rgName
+  location = var.location
+}
---
+++ Terraform/var.tf
@@ -0,0 +1,9 @@
+variable "rgName" {
+  type = string
+  default = "lirookRG"
+}
+
+variable "location" {
+  type = string
+  default = "West Europe"
+}
