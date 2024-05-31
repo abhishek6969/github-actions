d3c2d16b79fcf285fc92bd05801f5d6e18e378ca
abhishek6969 <sunkaleabhishek@gmail.com>
2024-05-31 17:46:16 +0530

    deploy

---
+++ Terraform/storage.tf
@@ -0,0 +1,21 @@
+resource "azurerm_storage_account" "testlirookstorage" {
+  name                     = "testlirookstorage"
+  resource_group_name      = azurerm_resource_group.testRG.name
+  location                 = azurerm_resource_group.testRG.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "testcontainer" {
+  name                  = "testcontainer"
+  storage_account_name  = azurerm_storage_account.testlirookstorage.name
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_blob" "testblob" {
+  name                   = "test.txt"
+  storage_account_name   = azurerm_storage_account.testlirookstorage.name
+  storage_container_name = azurerm_storage_container.testcontainer.name
+  type                   = "Block"
+  source                 = "test.txt"
+}
