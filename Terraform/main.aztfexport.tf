4013c9be6dc5ad7de03021807968eb94220c7b15
abhishek6969 <sunkaleabhishek@gmail.com>
2024-06-04 10:28:36 +0530

    deploy

--- Terraform/main.aztfexport.tf
+++ Terraform/main.aztfexport.tf
@@ -1,21 +1,9 @@
-# resource "azurerm_windows_virtual_machine" "res-0" {
-#   admin_password        = "Sunkale@14"
-#   admin_username        = "adminuser"
-#   location              = "northeurope"
-#   name                  = "test-linux"
-#   network_interface_ids = ["/subscriptions/bed9c8b2-bb60-492d-92a9-d1641fb7adf8/resourceGroups/test/providers/Microsoft.Network/networkInterfaces/test-linux103"]
-#   resource_group_name   = "test"
-#   size                  = "Standard_D4s_v3"
-#   additional_capabilities {
-#   }
-#   os_disk {
-#     caching              = "ReadWrite"
-#     storage_account_type = "Standard_LRS"
-#   }
-#   source_image_reference {
-#     offer     = "WindowsServer"
-#     publisher = "MicrosoftWindowsServer"
-#     sku       = "2022-datacenter-azure-edition"
-#     version   = "latest"
-#   }
+# resource "azurerm_storage_account" "res-0" {
+#   account_replication_type         = "LRS"
+#   account_tier                     = "Standard"
+#   allow_nested_items_to_be_public  = false
+#   cross_tenant_replication_enabled = false
+#   location                         = "northeurope"
+#   name                             = "testlirook"
+#   resource_group_name              = "test"
 # }
