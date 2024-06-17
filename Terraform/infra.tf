cd580d4985b79f8cdd9c097cfdd59b1a01d0902f
abhishek6969 <sunkaleabhishek@gmail.com>
2024-06-17 10:42:09 +0530

    deploy

--- Terraform/infra.tf
+++ Terraform/infra.tf
@@ -48,6 +48,10 @@ resource "azurerm_monitor_data_collection_rule" "example" {
     streams      = ["Microsoft-Perf"]
     destinations = ["example-log-analytics"]
   }
+  data_flow {
+    streams      = ["Microsoft-InsightsMetrics"]
+    destinations = ["example-metrics"]
+  }
 
   description = "Data collection rule for performance counters"
 }
